c36952033e36a232c87efa98138f52d76b7a33f5
maxwellbatis2 <lojasmaxlojas@gmail.com>
2025-06-24 09:58:24 -0300

    Initial commit: Baby Diary project

--- test-env.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-console.log('=== TESTE DE VARIÁVEIS DE AMBIENTE ===');
-console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
-console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY ? '***' + process.env.CLOUDINARY_API_KEY.slice(-4) : 'não definida');
-console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET ? '***' + process.env.CLOUDINARY_API_SECRET.slice(-4) : 'não definida');
-console.log('====================================='); 
